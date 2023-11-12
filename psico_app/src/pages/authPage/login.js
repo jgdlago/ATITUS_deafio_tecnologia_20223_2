2e7ae5036a7fe6a7c002c9d49cbbd4df7ad19420
João Dal Lago <50213926+jgdlago@users.noreply.github.com>
2023-11-12 01:25:45 -0300

    Auth method select

--- psico_app/src/pages/authPage/login.js
+++ psico_app/src/pages/authPage/login.js
@@ -1,30 +1,28 @@
-import './auth.css'
+import './auth.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
 
 function Login() {
-    return (
-      <div className="container-auth">
-
-        <div className='login-header'>
-          <span>
-          <FontAwesomeIcon icon={faChevronLeft} /> 
+  return (
+    <div className="container-auth">
+      <div className='login-header'>
+        <span>
+          <FontAwesomeIcon icon={faChevronLeft} />
           <b> Login</b>
-          <p>Olá, bem vindo de volta!</p>
-          </span>
-        </div>
-
-        <div className='select-auth-moethod'>
-            <span>Número</span>
-            <span>E-mail</span>
-        </div>
-
-        <div className='loginForm'>
-
-        </div>
-
+        </span>
+        <p>Olá, bem-vindo de volta!</p>
       </div>
-    );
-  }
-  
+
+      <div className='select-auth-method'>
+        <span>Número</span>
+        <span>E-mail</span>
+      </div>
+
+      <div className='login-form'>
+        
+      </div>
+    </div>
+  );
+}
+
 export default Login;
