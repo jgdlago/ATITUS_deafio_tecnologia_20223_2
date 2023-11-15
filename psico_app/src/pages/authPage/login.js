0f01f14213e040df6389c576d610f98492166d44
João Dal Lago <50213926+jgdlago@users.noreply.github.com>
2023-11-15 16:20:07 -0300

    login with email

--- psico_app/src/pages/authPage/login.js
+++ psico_app/src/pages/authPage/login.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import './auth.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
@@ -6,6 +7,12 @@ import SendButton from '../../components/button/send_button';
 import SocialAuth from '../../components/social_auth/social_auth';
 
 function Login() {
+  const [authMethod, setAuthMethod] = useState('number');
+
+  const handleMethodChange = (method) => {
+    setAuthMethod(method);
+  };
+
   return (
     <div className="container-auth">
       <div className='login-header'>
@@ -17,18 +24,31 @@ function Login() {
       </div>
 
       <div className='select-auth-method'>
-        <span>Número</span>
-        <span>E-mail</span>
+        <span onClick={() => handleMethodChange('number')}>Número</span>
+        <span onClick={() => handleMethodChange('email')}>E-mail</span>
       </div>
 
-      <div className='login-form'>
-        <form>
-          <img src={brasilIcon} alt="Brasil"/>
-          <input type="tel" id="phone" name="phone" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" placeholder="Número de telefone" required />
-        </form>
-        
-        <SendButton title='Enviar SMS'/>
-      </div>
+      {authMethod === 'number' && (
+        <div className='login-form-number'>
+          <form className='login-form'>
+            <img src={brasilIcon} alt="Brasil"/>
+            <input type="tel" id="phone" name="phone" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" placeholder="Número de telefone" required />
+          </form>
+          <SendButton title='Enviar SMS'/>
+        </div>
+      )}
+
+      {authMethod === 'email' && (
+        <div className='login-form-email'>
+          <form className='login-form'>
+
+            <input type="email" id="email" name="email" placeholder="E-mail" required />
+            <input type="password" id="password" name="password" placeholder="Senha" required />
+ 
+          </form>
+          <SendButton title='Login'/>
+        </div>
+      )}
 
       <SocialAuth title='Entrar com Google' />
 
