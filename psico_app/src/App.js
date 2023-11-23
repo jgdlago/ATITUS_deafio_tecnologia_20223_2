e9bc28bde9e6bfcd966645734c06ed893bc33684
João Dal Lago <50213926+jgdlago@users.noreply.github.com>
2023-11-22 21:52:37 -0300

    Button redirect

--- psico_app/src/App.js
+++ psico_app/src/App.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import './App.css';
 import Login from './pages/authPage/login';
 import Register from './pages/authPage/register';
@@ -5,15 +6,20 @@ import WelcomePage from './pages/welcomePage/welcomePage';
 import UsuarioDashboard from './pages/usuarioDashboard/UsuarioDashboard';
 import InclusaoMoodUsuario from './pages/inclusaoMoodUsuario/InclusaoMoodUsuario';
 
-
 function App() {
+  const [authAction, setAuthAction] = useState(null);
+
+  const handleAuthAction = (action) => {
+    setAuthAction(action);
+  };
+
   return (
     <div className="App">
-      <WelcomePage />
-      {/* <Login /> */}
-      {/* <Register /> */}
-      {/* <UsuarioDashboard /> */}
-      {/* <InclusaoMoodUsuario /> */}
+      {authAction === 'login' && <Login />}
+      {authAction === 'register' && <Register />}
+      {authAction === null && (
+        <WelcomePage onAuthAction={handleAuthAction} />
+      )}
     </div>
   );
 }
--- psico_app/src/pages/welcomePage/welcomePage.js
+++ psico_app/src/pages/welcomePage/welcomePage.js
@@ -1,34 +1,8 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './welcomePage.css';
 import AuthButton from '../../components/button/auth_button';
-import Login from '../authPage/login';
-import Register from '../authPage/register';
 
-function WelcomePage() {
-  const [activeComponent, setActiveComponent] = useState(null);
-
-  const handleButtonClick = (componentName) => {
-    setActiveComponent(componentName);
-
-    window.scrollTo({
-      top: document.body.scrollHeight,
-      behavior: 'smooth',
-    });
-  };
-
-  const renderActiveComponent = () => {
-    switch (activeComponent) {
-      case 'login':
-        return <Login />;
-      case 'login-psicologo':
-        return <Login />;
-      case 'register':
-        return <Register />;
-      default:
-        return null;
-    }
-  };
-
+function WelcomePage({ onAuthAction }) {
   return (
     <div className="welcomePage-container">
       <h2>Bem-vindo!</h2>
@@ -36,13 +10,9 @@ function WelcomePage() {
         <h3>MoodyCalendar</h3>
       </span>
 
-      <AuthButton title="Login paciente" onClick={() => handleButtonClick('login')} />
+      <AuthButton title="Login paciente" onClick={() => onAuthAction('login')} />
 
-      <AuthButton title="Login Psicólogo" onClick={() => handleButtonClick('login-psicologo')} />
-
-      <AuthButton title="Cadastro" onClick={() => handleButtonClick('register')} />
-
-      {renderActiveComponent()}
+      <AuthButton title="Cadastro" onClick={() => onAuthAction('register')} />
 
     </div>
   );
